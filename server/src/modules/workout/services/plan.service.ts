c776a8f7762b7db233fc12ed936a0721c18a4ac5
FelipeRinaldi1 <felipe.rinaldi.sobreira.dev0@gmail.com>
2026-03-23 09:14:15 -0300

    Feat/Plan: verify access in service, created controller and routes.

--- server/src/modules/workout/services/plan.service.ts
+++ server/src/modules/workout/services/plan.service.ts
@@ -1,29 +1,43 @@
 import { CreatePlanDTO, UpdatePlanDTO } from '../DTOs/plan.schema.js'
 import { PlanEntity } from '../entities/workoutPlan.entity.js'
 import { IPlanRepository } from '../interfaces/plan.interface.js'
-import { Result } from '@/@utils/result.js'
+import { failure, Result } from '@/@utils/result.js'
+import { WorkoutAccessControlService } from './accessControl.service.js'
+import { authenticatedUser } from '@/@shared/authenticatedUser.js'
 
 export class PlanService {
-	constructor(private planRepo: IPlanRepository) {}
+	constructor(
+		private planRepo: IPlanRepository,
+		private accessServ: WorkoutAccessControlService
+	) {}
 
-	async create(data: CreatePlanDTO, userId: string): Promise<Result<PlanEntity>> {
-		const result = await this.planRepo.create(data, userId)
+	async create(data: CreatePlanDTO, authUser: authenticatedUser): Promise<Result<PlanEntity>> {
+		const result = await this.planRepo.create(data, authUser.id)
 		return result
 	}
-	async update(id: string, data: UpdatePlanDTO, userId: string): Promise<Result<PlanEntity>> {
-		const result = await this.planRepo.update(id, data, userId)
+	async update(id: string, data: UpdatePlanDTO, authUser: authenticatedUser): Promise<Result<PlanEntity>> {
+		const access = await this.accessServ.canAccessPlan(id, authUser.id)
+		if (access.isFailure()) return failure(access.error)
+
+		const result = await this.planRepo.update(id, data, authUser.id)
 		return result
 	}
-	async delete(id: string, userId: string): Promise<Result<void>> {
-		const result = await this.planRepo.delete(id, userId)
+	async delete(id: string, authUser: authenticatedUser): Promise<Result<void>> {
+		const access = await this.accessServ.canAccessPlan(id, authUser.id)
+		if (access.isFailure()) return failure(access.error)
+
+		const result = await this.planRepo.delete(id, authUser.id)
 		return result
 	}
-	async findAll(userId: string): Promise<Result<PlanEntity[]>> {
-		const result = await this.planRepo.findAll(userId)
+	async findAll(authUser: authenticatedUser): Promise<Result<PlanEntity[]>> {
+		const result = await this.planRepo.findAll(authUser.id)
 		return result
 	}
-	async findById(id: string, userId: string): Promise<Result<PlanEntity>> {
-		const result = await this.planRepo.findById(id, userId)
+	async findById(id: string, authUser: authenticatedUser): Promise<Result<PlanEntity>> {
+		const access = await this.accessServ.canAccessPlan(id, authUser.id)
+		if (access.isFailure()) return failure(access.error)
+
+		const result = await this.planRepo.findById(id, authUser.id)
 		return result
 	}
 }
