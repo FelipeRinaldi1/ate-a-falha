0fd2b3ea0f1481a7595f34c8b37ee5d02bc78cf3
FelipeRinaldi1 <felipe.rinaldi.sobreira.dev0@gmail.com>
2026-04-11 20:30:35 -0300

    fix: remove dev containers and simplifying docker files and docker compose

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-	envDir: '../../',
-	plugins: [react()],
-	server: {
-		port: 3000,
-		host: true,
-	},
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, '../../', '')
+
+	return {
+		plugins: [react()],
+		server: {
+			port: parseInt(env.WEB_PORT || '3000'),
+			host: '0.0.0.0',
+		},
+	}
 })
